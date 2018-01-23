0427677a5ccc19898d72552223620210bc85ab04
Brian <Brraflor@outlook.com>
2018-01-23 09:06:56 -0800

    Created a login route w/ dummy login.html

--- gym/__init__.py
+++ gym/__init__.py
@@ -3,9 +3,12 @@ app = Flask(__name__)
 
 from gym.home.views import mod
 from gym.api.views import mod
+from gym.login.views import mod
 
 
 #Register the Home localhost:PORT/ route
 app.register_blueprint(home.views.mod, url_prefix='/')
 #Register the API route  localhost:PORT/api
 app.register_blueprint(api.views.mod, url_prefix='/api')
+#Register the Login route
+app.register_blueprint(login.views.mod, url_prefix='/login')
