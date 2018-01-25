0333a6b2d4d0ee6fbbb7296623055350b017edde
Brian <Brraflor@outlook.com>
2018-01-25 14:46:03 -0800

    tst

--- gym/api/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-mod = Blueprint('api', __name__)
-
-@mod.route('/tst')
-def test():
-    return '{tst : "You are in the api/tst"}'
--- run.py
+++
@@ -1,6 +0,0 @@
-from gym import app
-import os
-
-
-port = int(os.environ.get('PORT',8080))
-app.run(host='0.0.0.0', port=port, debug='TRUE')
