23c2846449f21b7e583bd6c5b557cc2476d70a29
Brian Flores <brraflor@outlook.com>
2018-01-26 16:14:11 -0800

    python to node

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var request = require('request');
+
+var port = 8080;
+var path = require('path');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+//app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+//Dont have to write ejs extension
+app.set('view engine', 'ejs');
+
+
+//Home Route
+app.get('/', function(req,res){
+  res.render('home');
+});
+
+app.get('/login', function(req,res){
+  res.render('login');
+});
+
+app.get('/loggedIn', function(req,res){
+  res.render('loggedIn');
+});
+
+
+
+
+
+app.listen(port, function() {
+  console.log('Server Started!\nPort:'+port);
+});
