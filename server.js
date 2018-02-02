cd899951bff4385d5b5714c50097d784305c1509
kyamasak <kyamasak@ucsc.edu>
2018-02-02 00:48:54 -0800

    routed route to journal.ejs

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ var request = require('request');
 var firebase = require("firebase");
 var port = 8080;
 var path = require('path');
-
 var app = express();
+
+var config = {
+  apiKey: "AIzaSyBA5J-Hn2Cl7GBxOZYBqb11B24ckd2yF1M",
+  authDomain: "gymapp-fd949.firebaseapp.com",
+  databaseURL: "https://gymapp-fd949.firebaseio.com",
+  projectId: "gymapp-fd949",
+  storageBucket: "gymapp-fd949.appspot.com",
+  messagingSenderId: "871486720421"
+};
+var fb = firebase.initializeApp(config);
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -26,6 +36,10 @@ app.get('/login', function(req,res){
 
 app.get('/login/loggedIn', function(req,res){
   res.redirect('/');
+});
+
+app.get('/journal', function(req,res){
+  res.render('journal');
 });
 
 
