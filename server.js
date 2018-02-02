0fb3a015d08ce05669d72bf8bbc44e7f31e6c3ce
Brian <Brraflor@outlook.com>
2018-02-02 08:47:54 -0800

    ready for back end

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = 8080;
 var path = require('path');
 
 var app = express();
-
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 //app.use(express.static('public'));
@@ -24,8 +24,34 @@ app.get('/login', function(req,res){
   res.render('login');
 });
 
-app.get('/login/loggedIn', function(req,res){
+app.get('/loggedIn', function(req,res){
   res.render('loggedIn');
+});
+
+app.get('/profilepage', function(req,res){
+  res.render('profilepage');
+});
+
+app.post("/tst", (req, res) => {
+  var data= req.body;
+  //name
+  var name = data.first;
+  var last = data.last;
+  var age = data.age;
+  var address = data.address;
+  var city = data.city;
+  var state = data.state;
+  var zip = data.zip;
+
+  console.log(name);
+  console.log(last);
+  console.log(age);
+  console.log(address);
+  console.log(city);
+  console.log(state);
+  console.log(zip);
+  res.send("printed")
+
 });
 
 
