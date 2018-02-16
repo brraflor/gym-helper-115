f450d3f8856f3dbd8ca7f10aac4ffb6b12d2ba9f
Brian <Brraflor@outlook.com>
2018-02-16 15:07:28 -0800

    cleaning html files

---
+++ public/js/initFirebase.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyBA5J-Hn2Cl7GBxOZYBqb11B24ckd2yF1M",
+  authDomain: "gymapp-fd949.firebaseapp.com",
+  databaseURL: "https://gymapp-fd949.firebaseio.com",
+  projectId: "gymapp-fd949",
+  storageBucket: "gymapp-fd949.appspot.com",
+  messagingSenderId: "871486720421"
+};
+firebase.initializeApp(config);
